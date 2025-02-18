9ea3f2b6e49d511fd44cb12b6be09dfade29f844
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-18 13:58:29 +0700

    add view commons

---
+++ src/components/view/common/List.ts
@@ -0,0 +1,60 @@
+import { View } from '../../base/View';
+
+import {
+	ElementsMap,
+	ItemData,
+	ListData,
+	ListSettings,
+} from '@/types/components/view/common/List';
+
+/**
+ * Класс для отображения списка элементов
+ */
+export class ListView<T extends ItemData> extends View<ListData<T>, ListSettings<T>> {
+	// Сохраняем элементы в объекте, где ключ - id элемента
+	protected _elements: ElementsMap;
+
+	/**
+	 * Устанавливаем активный элемент
+	 */
+	setActiveElement(element: HTMLElement) {
+		const elements = Object.values(this._elements);
+		if (elements.includes(element)) {
+			elements.map((element) =>
+				element.classList.remove(this.settings.activeItemClass)
+			);
+			element.classList.add(this.settings.activeItemClass);
+		}
+	}
+
+	/**
+	 * Устанавливаем активный элемент по id
+	 */
+	setActiveItem(id: string) {
+		if (this._elements[id]) {
+			this.setActiveElement(this._elements[id]);
+		}
+	}
+
+	/**
+	 * Обновляем отображение списка элементов
+	 */
+	set items(items: T[]) {
+		// console.log(items); тут стали [{}, {}, {}, {}]
+		this._elements = items.reduce<ElementsMap>((result, item) => {
+			// Копируем заранее настроенное отображение
+			const el = this.settings.item.copy();
+			// console.log(el);
+			// console.log(this.settings.item.copy());
+			// Добавляем класс элемента
+			el.element.classList.add(this.settings.itemClass);
+			// Заполняем нужными данными и сохраняем в объекте
+			// console.log(el);
+			// console.log(item);
+			result[item.id] = el.render(item); // сюда передаётся item (как модель)
+			return result;
+		}, {});
+		// console.log(this._elements); // тут уже готовый список элементов [{}, {}, {}, {}]
+		this.setValue(this.element, Object.values(this._elements));
+	}
+}
